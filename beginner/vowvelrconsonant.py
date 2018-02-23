b9fa1029e8802a7984f40a2c97f5c79a4d126ab1
karthikgoli74 <36730618+karthikgoli74@users.noreply.github.com>
2018-02-23 14:46:54 +0530

    Update vowvelrconsonant.py

--- beginner/vowvelrconsonant.py
+++ beginner/vowvelrconsonant.py
@@ -2,4 +2,4 @@ a=raw_input()
 if(a=='a' or a=='e' or a=='i' or a=='o' or a=='u' or a=='A' or a=='E' or a=='I' or a=='O' or a=='U'):
   print("the letter is vowvel")
 else:
-  print("the letter is not a vowvel")
+  print("the letter is Consonant")
