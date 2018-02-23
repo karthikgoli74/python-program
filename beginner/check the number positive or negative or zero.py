04d2b1e1cab8df00c783ed206e77c7726b8918e1
karthikgoli74 <36730618+karthikgoli74@users.noreply.github.com>
2018-02-23 14:05:07 +0530

    Update and rename check the number positive or negative or zero.py to positive.py

--- beginner/check the number positive or negative or zero.py
+++
@@ -1,7 +0,0 @@
-b=input()
-if(b>0):
-    print("the number is positive")
-elif(b<0):
-    print("the number is negative")
-elif(b==0):
-    print("the number is zero")
---
+++ beginner/positive.py
@@ -0,0 +1,7 @@
+a=input()
+if(a>0):
+  print("the value is positive")
+elif(a<0):
+    print("the value is negative")
+elif(a==0):
+    print("the value is zero")
